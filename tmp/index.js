d67820bc11616062c08ba942d9f4c48452b69cb5
jincdream <275727449@qq.com>
2015-12-16 12:24:24 +0800

    fix path
    fix path

--- tmp/index.js
+++ tmp/index.js
@@ -6,11 +6,13 @@ const path = require('path')
 
 class Templates {
   constructor (key,viewDir){
-    let dirs = fs.readdirSync(viewDir)
+    let _vD = path.resolve(__dirname,'../',viewDir)
+    let dirs = fs.readdirSync(_vD)
     let views ={}
     let tmpPath = {}
     dirs.map((file,i) => {
-      let _path = path.join(viewDir,file)
+      let _path = path.resolve(_vD,file)
+      console.log(_path)
       let data = fs.readFileSync(_path)
       views[file] = '' + data
       tmpPath[file] = _path
